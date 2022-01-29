dbe1db62eb5bfd749fab8cc22db3917a608e3fc5
Daniel Higginson <danbobhig@gmail.com>
2022-01-29 19:11:53 +0000

    add templates, base.html and home.html, tested with simple css style

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -7,12 +12,21 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/get_customer")
+def get_customer():
+    
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
