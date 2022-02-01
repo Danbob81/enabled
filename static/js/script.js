1136787eee5a675deb5c72365bf4a51b6cfb985f
Daniel Higginson <danbobhig@gmail.com>
2022-02-01 21:31:19 +0000

    create modal containing request form

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
     $(".sidenav").sidenav({edge: "left"});
+    $('.modal').modal();
+    $('.datepicker').datepicker({
+      format: "dd mmmm yyyy",
+      yearRange: [1900, 2021],
+      showClearBtn: true,
+      i18n: {
+        done: "Select"
+      }
+    });
+    $('select').formSelect();
     $("#copyright").text(new Date().getFullYear());
   });
