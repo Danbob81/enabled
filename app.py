93ad6ffdd1de863297c469fb2233ac5a3dca5a89
Daniel Higginson <danbobhig@gmail.com>
2022-02-11 10:15:28 +0000

    add log out functionality

--- app.py
+++ app.py
@@ -37,11 +37,11 @@ def login():
         if existing_user:
             # check hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    # flash("Welcome, {}".format(
-                    #     request.form.get("username")))
-                    return redirect(url_for(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                # flash("Welcome, {}".format(
+                #     request.form.get("username")))
+                return redirect(url_for(
                         "login", username=session["user"]))
             else:
                 # password doesn't match
@@ -56,10 +56,12 @@ def login():
     return render_template("account.html")
 
 
-# @app.route("/account", methods=["POST", "GET"])
-# def account():
-#     """render account page"""
-#     return render_template("account.html")
+@app.route("/logout")
+def logout():
+    """log user out of account"""
+    flash("You have logged out!")
+    session.pop("user")
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
